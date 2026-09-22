7bfc56697f7bfe35a0a7cff44cf81c623cdb1783
Deadpool-a <sobiiiiiii.14@gmail.com>
2026-09-22 20:57:17 +0530

    Play goodbye GIF only after the closing message ends

--- script/components/closing.js
+++ script/components/closing.js
@@ -12,13 +12,13 @@
         <p class="replay-btn" id="replay">${
           section.replayText || "Or click, if you want to watch it again."
         }</p>
+        <p class="last-smile">:)</p>
         ${section.byeGif ? `
           <div class="tenor-gif-embed" data-postid="12810370250826095008" data-share-method="host" data-aspect-ratio="1.33871" data-width="100%">
             <a href="https://tenor.com/view/bye-waving-goodbye-little-rascals-see-you-gif-12810370250826095008">Bye Waving GIF</a>
             from <a href="https://tenor.com/search/bye-gifs">Bye GIFs</a>
           </div>
         ` : ""}
-        <p class="last-smile">:)</p>
       `;
       container.appendChild(div);
 
@@ -36,14 +36,26 @@
 
     animate(tl, el) {
       const ideaIn = { opacity: 0, y: -20, rotationX: 5, skewX: "15deg" };
-      tl.from(el.querySelectorAll("p, .tenor-gif-embed"), {
-        duration: 1, ...ideaIn, stagger: 1.2,
+      const gif = el.querySelector(".tenor-gif-embed");
+
+      // Show the goodbye GIF only after the complete closing message.
+      tl.from(el.querySelectorAll(".closing-text, #replay, .last-smile"), {
+        duration: 1,
+        ...ideaIn,
+        stagger: 1.2,
       })
-      // Enable replay button only after it becomes visible
-      .set(el.querySelector("#replay"), { pointerEvents: "auto" })
-      .to(el.querySelector(".last-smile"), {
-        duration: 0.5, rotation: 90,
-      }, "+=1");
+        .set(el.querySelector("#replay"), { pointerEvents: "auto" })
+        .to(el.querySelector(".last-smile"), {
+          duration: 0.5,
+          rotation: 90,
+        }, "+=1");
+
+      if (gif) {
+        tl.from(gif, {
+          duration: 1,
+          ...ideaIn,
+        }, "+=0.5");
+      }
     },
   };
 })();
